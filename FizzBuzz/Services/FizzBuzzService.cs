9400bb5c4161f349f85ccab80854e42831fd6352
DialloAbdou <=dialloabdou@hotmail.com>
2023-10-29 21:21:33 +0100

    realisation du prémier test automatiser sur FIzzBuzz

---
+++ FizzBuzz/Services/FizzBuzzService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Services
+{
+    public class FizzBuzzService
+    {
+        /// <summary>
+        /// elle renvoie un string 
+        /// si la valeur est divisible par 3 ou par 5
+        /// </summary>
+        /// <param name="valeur"></param>
+        /// <returns></returns>
+        public string getFizzBuzz(int valeur)
+        {
+            if (valeur % 3 == 0 && valeur % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (valeur % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (valeur % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return valeur.ToString();
+            }
+
+        }
+    }
+}
---
+++ FizzBuzzTest/Services/FizzBuzzServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using FizzBuzz.Services;
+
+namespace FizzBuzzTest.Services
+{
+    public class FizzBuzzServiceTest
+    {
+        [Fact]
+        public void getFizzBuzz_should_return_Fizz_when_value_divisible_by_three()
+        {
+            // Arrange
+            var service = new FizzBuzzService();
+            var expected = "Fizz";
+            //Act
+            var result = service.getFizzBuzz(3);
+            //Assert
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void getFizzBuzz_should_return_BuZZ_when_value_divisible_by_five()
+        {
+            // 1. Arrange => Préparer mon environnement
+            var service = new FizzBuzzService();
+
+            //2. Act => Executer la methode ou l'unite business dont je teste
+            var result = service.getFizzBuzz(5);
+            //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void getFizzBuzz_should_return_FIzzBuZ_when_value_divisible_by_Three_And_Five()
+        {
+            // 1. Arrange => Préparer mon environnement
+            var service = new FizzBuzzService();
+
+            //2. Act => Executer la methode ou l'unite business dont je teste
+            var result = service.getFizzBuzz(30);
+            //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
+            Assert.Equal("FizzBuzz", result);
+        }
+
+        [Fact]
+        public void getFizzBuzz_should_return_2_if_2_Passed()
+        {
+            // 1. Arrange => Préparer mon environnement
+            var service = new FizzBuzzService();
+
+            //2. Act => Executer la methode ou l'unite business dont je teste
+            var result = service.getFizzBuzz(2);
+            //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
+            Assert.Equal("2", result);
+        }
+
+    }
+}
