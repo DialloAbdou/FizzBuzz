1c74d7af82a6184d177468edb340b9b0f712dfe1
DialloAbdou <=dialloabdou@hotmail.com>
2023-10-31 15:24:15 +0100

    initialisation du test

--- tests/FizzBuzzTest/Services/FizzBuzzServiceTest.cs
+++ tests/FizzBuzzTest/Services/FizzBuzzServiceTest.cs
@@ -4,11 +4,15 @@ namespace FizzBuzzTest.Services
 {
     public class FizzBuzzServiceTest
     {
+        FizzBuzzService service;
+        public FizzBuzzServiceTest()
+        {
+            service = new FizzBuzzService();
+        }
         [Fact]
         public void getFizzBuzz_should_return_Fizz_when_value_divisible_by_three()
         {
             // Arrange
-            var service = new FizzBuzzService();
             var expected = "Fizz";
             //Act
             var result = service.GetFizzBuzz(3);
@@ -19,8 +23,6 @@ namespace FizzBuzzTest.Services
         public void getFizzBuzz_should_return_BuZZ_when_value_divisible_by_five()
         {
             // 1. Arrange => Préparer mon environnement
-            var service = new FizzBuzzService();
-
             //2. Act => Executer la methode ou l'unite business dont je teste
             var result = service.GetFizzBuzz(5);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
@@ -31,8 +33,6 @@ namespace FizzBuzzTest.Services
         public void getFizzBuzz_should_return_FIzzBuZ_when_value_divisible_by_Three_And_Five()
         {
             // 1. Arrange => Préparer mon environnement
-            var service = new FizzBuzzService();
-
             //2. Act => Executer la methode ou l'unite business dont je teste
             var result = service.GetFizzBuzz(15);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
@@ -43,7 +43,7 @@ namespace FizzBuzzTest.Services
         public void getFizzBuzz_should_return_2_if_2_Passed()
         {
             // 1. Arrange => Préparer mon environnement
-            var service = new FizzBuzzService();
+
             //2. Act => Executer la methode ou l'unite business dont je teste
             var result = service.GetFizzBuzz(2);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
@@ -55,9 +55,9 @@ namespace FizzBuzzTest.Services
         public void GetFizzBuzzPhrase_should_return_FizzBuzzPhrase()
         {
             // 1. Arrange => Préparer mon environnement
-            var service = new FizzBuzzService();
+
             //2. Act => Executer la methode ou l'unite business dont je teste
-            var result = service.GetFizzBuzz(15);
+            var result = service.GetFizzBuzzPhrase(15);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
             Assert.Equal("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz", result);
         }
