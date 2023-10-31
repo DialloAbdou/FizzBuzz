1b81626007035fd30c452ac96486f104c0757f37
DialloAbdou <=dialloabdou@hotmail.com>
2023-10-31 11:57:48 +0100

    Modification sur la structure du projet

---
+++ src/FizzBuzz/Services/FizzBuzzService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Services
+{
+    public class FizzBuzzService
+    {
+        /// <summary>
+        /// elle renvoie un string 
+        /// si la valeur est divisible par 3 ou par 5
+        /// </summary>
+        /// <param name="valeur"></param>
+        /// <returns></returns>
+        public string GetFizzBuzz(int valeur)
+        {
+            string result = string.Empty;
+            if (valeur % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (valeur % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            if (string.IsNullOrWhiteSpace(result))
+            {
+
+                result += valeur.ToString();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string GetFizzBuzzPhrase(int max)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i<= max; i++)
+            {
+               sb.Append(GetFizzBuzz(i));
+                if(i != max)
+                {
+                    sb.Append(" ");
+                }
+             }
+            return sb.ToString();
+        }
+    }
+}
--- tests/FizzBuzzTest/Services/FizzBuzzServiceTest.cs
+++ tests/FizzBuzzTest/Services/FizzBuzzServiceTest.cs
@@ -11,7 +11,7 @@ namespace FizzBuzzTest.Services
             var service = new FizzBuzzService();
             var expected = "Fizz";
             //Act
-            var result = service.getFizzBuzz(3);
+            var result = service.GetFizzBuzz(3);
             //Assert
             Assert.Equal(expected, result);
         }
@@ -22,7 +22,7 @@ namespace FizzBuzzTest.Services
             var service = new FizzBuzzService();
 
             //2. Act => Executer la methode ou l'unite business dont je teste
-            var result = service.getFizzBuzz(5);
+            var result = service.GetFizzBuzz(5);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
             Assert.Equal("Buzz", result);
         }
@@ -34,7 +34,7 @@ namespace FizzBuzzTest.Services
             var service = new FizzBuzzService();
 
             //2. Act => Executer la methode ou l'unite business dont je teste
-            var result = service.getFizzBuzz(30);
+            var result = service.GetFizzBuzz(15);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
             Assert.Equal("FizzBuzz", result);
         }
@@ -44,11 +44,22 @@ namespace FizzBuzzTest.Services
         {
             // 1. Arrange => Préparer mon environnement
             var service = new FizzBuzzService();
-
             //2. Act => Executer la methode ou l'unite business dont je teste
-            var result = service.getFizzBuzz(2);
+            var result = service.GetFizzBuzz(2);
             //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
             Assert.Equal("2", result);
+        }
+
+
+        [Fact]
+        public void GetFizzBuzzPhrase_should_return_FizzBuzzPhrase()
+        {
+            // 1. Arrange => Préparer mon environnement
+            var service = new FizzBuzzService();
+            //2. Act => Executer la methode ou l'unite business dont je teste
+            var result = service.GetFizzBuzz(15);
+            //3. Assert => je verifie suite à l'execution ma methode ou l'unite bussiness j'ai le resultat attendu
+            Assert.Equal("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz", result);
         }
 
     }
